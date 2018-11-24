71b24393962e87d31c1779f29122a86772fc7ada
William King <williamlongking@hotmail.com>
2018-11-24 10:28:51 -0500

    Initial Commit
    Layout created

---
+++ Medicord2/app/src/main/java/com/kingapps/medicord/fileActivity.java
@@ -0,0 +1,13 @@
+package com.kingapps.medicord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class fileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file);
+    }
+}
